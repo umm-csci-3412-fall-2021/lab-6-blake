d76dbd7d4e49638d790cc895faf048439d8a3dd7
Blake Johnson <joh18484@tardis.morris.umn.edu>
2021-11-02 10:17:27 -0500

    Edited classes in both java files Moved created classes into the main public class for each file and passed instances of each into contructors with created, seperate threads. Both java files compile without error.

--- src/echoserver/EchoClient.java
+++ src/echoserver/EchoClient.java
@@ -5,70 +5,68 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
 
-public class InputReader implements Runnable {
-
-	private final Socket socket;
-
-	public static void Main(Socket socket) {
-		InputReader obj = new InputReader();
-		Thread inThread = new Thread(obj);
-		inThread.start();
-	}
-	
-	public void run() {
-
-		try {
-			InputStream stdIn = System.in;
-			OutputStream socketOut = socket.getOutputStream();
-
-			int data;
-			while ((data = stdIn.read()) != -1) {
-				socketOut.write(data);
-				socketOut.flush();
-			}
-		}
-
-		catch (IOException e) {
-            e.printStackTrace();
-        }
-	}	
-}
-
-public class OutputWriter implements Runnable {
-
-	private final Socket socket;
-	
-	public static void Main(Socket socket) {
-		OutputWriter obj = new OutputWriter();
-		Thread outThread = new Thread(obj);
-		outThread.start();
-	}
-
-	public void run() {
-
-		try {
-			InputStream socketIn = socket.getInputStream();
-			OutputStream stdOut = System.out;
-
-			int data;
-			while((data = socketIn.read()) != -1) {
-				stdOut.write();
-				stdOut.flush();
-			}
-		}
-
-		catch (IOException e) {
-            e.printStackTrace();
-        }	
-	}
-}
-
 public class EchoClient {
+	
 	public static final int PORT_NUMBER = 6013;
 
 	public static void main(String[] args) throws IOException {
 		EchoClient client = new EchoClient();
 		client.start();
+	}
+
+	class InputReader implements Runnable {
+
+		private final Socket socket;
+
+		public InputReader(Socket socket){
+		this.socket = socket;
+		}
+	
+		public void run() {
+
+			try {
+				InputStream stdIn = System.in;
+				OutputStream socketOut = socket.getOutputStream();
+
+				int data;
+				while ((data = stdIn.read()) != -1) {
+					socketOut.write(data);
+					socketOut.flush();
+				}
+			}
+
+			catch (IOException e) {
+            e.printStackTrace();
+       		}
+		}	
+	}
+
+	class OutputWriter implements Runnable {
+
+		private final Socket socket;
+
+		public OutputWriter(Socket socket){
+		this.socket = socket;
+		}
+
+
+		public void run() {
+
+			try {
+				InputStream socketIn = socket.getInputStream();
+				OutputStream stdOut = System.out;
+
+				int data;
+				while((data = socketIn.read()) != -1) {
+					stdOut.write(data);
+					stdOut.flush();
+				}
+			}
+
+			catch (IOException e) {
+            e.printStackTrace();
+       		}	
+		}
 	}
 
 	private void start() throws IOException {
@@ -77,5 +75,15 @@ public class EchoClient {
 		OutputStream socketOutputStream = socket.getOutputStream();
 
 		// Put your code here.
+		while(true) {
+
+			InputReader input = new InputReader(socket);
+			Thread inThread = new Thread(input);
+			inThread.start();
+
+			OutputWriter output = new OutputWriter(socket);
+			Thread outThread = new Thread(output);
+			outThread.start();
+		}
 	}
 }
--- src/echoserver/EchoServer.java
+++ src/echoserver/EchoServer.java
@@ -6,42 +6,41 @@ import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-
-public class SocketStream implements Runnable {
-	
-	private final Socket socket;
-	
-	public SocketStream(Socket socket){
-		this.socket = socket;
-	}
-
-	public void run () {
-
-		try {
-			InputStream socketIn = socket.getInputStream();
-			OutputStream socketOut = socket.getOutputStream(); 
-
-			int data;
-			while((data = socketIn.read()) != -1) {
-				socketOut.write(data);
-				socketOut.flush();
-			}
-		}
-
-		catch (IOException e) {
-            e.printStackTrace();
-        }
-	}
-}
-
 public class EchoServer {
 	
-	
 	public static final int PORT_NUMBER = 6013; 
+
 	public static void main(String[] args) throws IOException, InterruptedException {
 		EchoServer server = new EchoServer();
 		server.start();
 	}
+
+	class SocketStream implements Runnable {
+	
+		private final Socket socket;
+	
+		public SocketStream(Socket socket){
+		this.socket = socket;
+		}
+
+		public void run() {
+
+			try {
+				InputStream socketIn = socket.getInputStream();
+				OutputStream socketOut = socket.getOutputStream(); 
+
+				int data;
+				while((data = socketIn.read()) != -1) {
+					socketOut.write(data);
+					socketOut.flush();
+				}
+			}
+
+			catch (IOException e) {
+        	e.printStackTrace();
+       		}
+		}
+	} 
 
 	private void start() throws IOException, InterruptedException {
 		ServerSocket serverSocket = new ServerSocket(PORT_NUMBER);
@@ -50,10 +49,10 @@ public class EchoServer {
 
 			// Put your code here.
 			// This should do very little, essentially:
-			SocketStream obj = new SocketStream();// * Construct an instance of your runnable class
-			Thread thread = new Thread(obj);// * Construct a Thread with your runnable
+			SocketStream stream = new SocketStream(socket); // * Construct an instance of your runnable class
+			Thread thread = new Thread(stream); // * Construct a Thread with your runnable
 			// * Or use a thread pool
-			thread.start();// * Start that thread
+			thread.start(); // * Start that thread
 		}
 	}
 }
